1439fab2d781ec9ab5066d8fb2a2f7841e0182f0
Augusto Valerio <augusto.valerio06@gmail.com>
2026-05-19 21:17:32 -0300

    chore: criar estrutura inicial do projeto

---
+++ src/br/com/totvs/insightmeet/Main.java
@@ -0,0 +1,8 @@
+package br.com.totvs.insightmeet;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("InsightMeet TOTVS");
+        System.out.println("Sistema iniciado com sucesso.");
+    }
+}
